0bc6e79359090b06e818f54117780591690ad1e0
jokerboozp <2929590482@qq.com>
2023-07-31 14:30:21 +0800

    v0.3request消息请求的封装

---
+++ ziface/IRequest.go
@@ -0,0 +1,13 @@
+package ziface
+
+/**
+IRequest接口：
+实际上是把客户端请求的链接信息和请求的数据包装到了一个Request中
+*/
+
+type IRequest interface {
+	// GetConnection 得到当前请求
+	GetConnection() IConnection
+	// GetData 得到请求的消息数据
+	GetData() []byte
+}
